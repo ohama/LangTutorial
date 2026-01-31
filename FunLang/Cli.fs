188cc56e6dbdde0b1530665cbd3bc56dee73b629
ohama <ohama100@gmail.com>
2026-02-01 00:45:12 +0900

    feat(03-01): add Argu package and CLI argument type
    - Add Argu 6.2.5 package reference
    - Create Cli.fs with CliArgs discriminated union
    - Define CLI flags: --expr/-e, --emit-tokens, --emit-ast, --emit-type, --repl
    - Argu auto-converts Emit_Tokens to --emit-tokens flag

---
+++ FunLang/Cli.fs
@@ -0,0 +1,22 @@
+module Cli
+
+open Argu
+
+[<CliPrefix(CliPrefix.DoubleDash)>]
+type CliArgs =
+    | [<AltCommandLine("-e")>] Expr of expression: string
+    | Emit_Tokens
+    | Emit_Ast
+    | Emit_Type
+    | Repl
+    | [<MainCommand; Last>] File of filename: string
+with
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Expr _ -> "evaluate expression"
+            | Emit_Tokens -> "show lexer tokens"
+            | Emit_Ast -> "show parsed AST"
+            | Emit_Type -> "show inferred types (reserved)"
+            | Repl -> "start interactive REPL"
+            | File _ -> "evaluate program from file"
